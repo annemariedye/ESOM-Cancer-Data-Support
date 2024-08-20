b7fbd06ad06d27538f510d4bd065b541f638cc92
Anne Marie Dye <anne.marie.dye@emory.edu>
2024-08-20 14:50:37 -0400

    original code for boxplot in #, corrected bar chart for frequency of MMR positives for each cancer stage by BMI category

---
+++ R/stagesimple_OLR.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+library(dplyr)
+
+data <- AEH_R_Data
+
+ggplot(data, aes(x = stage_simple, y = MMR, group = Cat_BMI)) +
+	geom_col() +
+	# geom_boxplot(size = .75) +
+	# geom_point(alpha = .5) +
+	facet_grid(. ~ Cat_BMI, margins = TRUE) +
+	labs(
+		title = "Relationship of MMR Status and Cancer Stage by BMI Category",
+		x = "Cancer Stage",
+		y = "MMR Positive",
+		caption = "BMI Categories are coded as 0 < 25, 1 = 25-29.9, 2 = 30-34.9, 3 = 35-39.9, 4 ≥ 40"
+	) +
+	theme_minimal() +
+	theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
