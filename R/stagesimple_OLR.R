15f9cf514011a0a0b3f5213fda10ea7823c322db
Anne Marie Dye <anne.marie.dye@emory.edu>
2024-08-21 14:48:01 -0400

    added notes on when to use which lines of code altered axis title for MMR added a figure folder

--- R/stagesimple_OLR.R
+++ R/stagesimple_OLR.R
@@ -4,14 +4,14 @@ library(dplyr)
 data <- AEH_R_Data
 
 ggplot(data, aes(x = stage_simple, y = MMR, group = Cat_BMI)) +
-	geom_col() +
-	# geom_boxplot(size = .75) +
-	# geom_point(alpha = .5) +
+	geom_col() + #use for bar graph
+	#geom_boxplot(size = .75) + #use for box plot
+	#geom_point(alpha = .5) + #use for box plot
 	facet_grid(. ~ Cat_BMI, margins = TRUE) +
 	labs(
-		title = "Relationship of MMR Status and Cancer Stage by BMI Category",
+		title = "Relationship Between MMR Status and Cancer Stage by BMI Category",
 		x = "Cancer Stage",
-		y = "MMR Positive",
+		y = "MMR Proficient",
 		caption = "BMI Categories are coded as 0 < 25, 1 = 25-29.9, 2 = 30-34.9, 3 = 35-39.9, 4 ≥ 40"
 	) +
 	theme_minimal() +
