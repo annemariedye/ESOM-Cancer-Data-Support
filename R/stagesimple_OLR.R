a8a1b354a481f436e5616037c86453cb40623939
Anne Marie Dye <anne.marie.dye@emory.edu>
2024-08-28 16:54:33 -0400

    added OLR

--- R/stagesimple_OLR.R
+++ R/stagesimple_OLR.R
@@ -1,13 +1,13 @@
 library(ggplot2)
 library(dplyr)
 
-data <- AEH_R_Data
+data <- AEH_data
 
-ggplot(data, aes(x = stage_simple, y = MMR, group = Cat_BMI)) +
+ggplot(data, aes(x = stage_simple, y = MMR, group = BMI_cat)) +
 	geom_col() + #use for bar graph
 	#geom_boxplot(size = .75) + #use for box plot
 	#geom_point(alpha = .5) + #use for box plot
-	facet_grid(. ~ Cat_BMI, margins = TRUE) +
+	facet_grid(. ~ BMI_cat, margins = TRUE) +
 	labs(
 		title = "Relationship Between MMR Status and Cancer Stage by BMI Category",
 		x = "Cancer Stage",
@@ -16,3 +16,24 @@ ggplot(data, aes(x = stage_simple, y = MMR, group = Cat_BMI)) +
 	) +
 	theme_minimal() +
 	theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+library(MASS)
+
+data$stage_simple <- factor(data$stage_simple,
+														levels = c(0:4),
+														ordered = TRUE)
+
+olr_model <- polr(stage_simple ~ MMR + BMI_cat, data = data, Hess = TRUE)
+
+summary(olr_model)
+
+# Get the coefficients table from the summary
+ctable <- coef(summary(olr_model))
+
+# Calculate p-values using the Wald test (two-tailed)
+p_values <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+
+# Combine the coefficients, standard errors, t-values, and p-values into a table
+results <- cbind(ctable, "p value" = p_values)
+print(results)
+
